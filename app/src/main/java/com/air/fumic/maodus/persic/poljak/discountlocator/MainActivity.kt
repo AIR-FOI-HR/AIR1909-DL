2303830e5c636dc53d0e5bbf2616f2eca869661c
vpoljak <vpoljak@foi.hr>
2020-01-30 19:39:10 +0100

    Push notifictation added

--- app/src/main/java/com/air/fumic/maodus/persic/poljak/discountlocator/MainActivity.kt
+++ app/src/main/java/com/air/fumic/maodus/persic/poljak/discountlocator/MainActivity.kt
@@ -2,7 +2,12 @@ package com.air.fumic.maodus.persic.poljak.discountlocator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.air.fumic.maodus.persic.poljak.database.MyDb
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.iid.FirebaseInstanceId
+import com.google.firebase.messaging.FirebaseMessaging
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,5 +18,20 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         database.selectUsers()
+        notificationSetup()
+    }
+
+    fun notificationSetup() {
+        FirebaseMessaging.getInstance().isAutoInitEnabled = true
+
+        FirebaseInstanceId.getInstance().instanceId
+            .addOnCompleteListener(OnCompleteListener { task ->
+                if (!task.isSuccessful) {
+                    return@OnCompleteListener
+                }
+
+                val token = task.result?.token
+                //Toast.makeText(baseContext, token, Toast.LENGTH_SHORT).show()
+            })
     }
 }
